a30d3044ee032001f5a0ba3804ff958dc5b32f84
ConorCurry <conorcurry@gmail.com>
2016-10-16 20:20:06 +0200

    Changed to integer dice rolling algorithm for determining MapTile values. Performance is down now, though

--- agro.jl
+++ agro.jl
@@ -1,38 +1,44 @@
-#Initialize Game World
+module AgroGame
 
 type MapTile{T}
   moisture::T
   fertility::T
 end
 
-function scaledRandn(st::Real, mu::Real, T::DataType)::T
-  r = zero(Float16)
-  while true
-    r = randn(Float16) * st + mu
-    typemin(T) < r < typemax(T) && break
+#gets a random value weighted based on rolls
+#random values are on scale of 0-255
+#uses accept/reject => higher lucks take longer
+#luck is nonlinear. numRolls dictates uniformity
+function getRandVal(luck, numRolls=2)
+  hiVal = Int(typemax(UInt8))
+  m = hiVal/numRolls
+  rolls = Array{UInt8}(numRolls)
+  for i in 1:numRolls
+    while true
+      rolls[i] = rand(0:m)
+      if luck != 0
+        accRejVal = Int(rand(0:hiVal))
+        y = -(luck/hiVal)*rolls[i]*numRolls + luck
+        accRejVal > y && break
+      else
+        break
+      end
+    end
   end
-  x = trunc(T, r)
-  return x
+  return UInt8(sum(rolls))
 end
 
-function randmap(x::Int, y::Int, T=UInt8::DataType)
-  lo = typemin(T)
-  hi = typemax(T)
-  mu = (hi - lo) / 2
-  st = mu / 3
-  genMap = Array{MapTile{T}}(x, y)
-  for i in 1:length(genMap)
-    genMap[i] = MapTile{T}(scaledRandn(st, mu, T), scaledRandn(st, mu, T))
-    #bound moisture
-    while !(lo < genMap[i].moisture < hi)
-      genMap[i].moisture = scaledRandn(st, mu, T)
-    end
-    #bound fertility
-    while !(lo < genMap[i].fertility < hi)
-      genMap[i].fertility = scaledRandn(st, mu, T)
-    end
-  end
+#@show mean([getRandVal(1024,4) for i in 1:1000])
+
+function randmap(x::Int, y::Int, luck=0, T=UInt8::DataType)
+  lo = typemin(T); hi = typemax(T)
+  genMap = [MapTile{T}(getRandVal(luck,2), getRandVal(luck,2)) for xdim in 1:x, ydim in 1:y]
   return genMap
 end
 
-@time boundedNorm = randmap(100, 100)
+@time boundedNorm = randmap(1000, 1000)
+#@show boundedNorm
+@show typeof(boundedNorm), size(boundedNorm)
+
+
+end #of module
