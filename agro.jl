1706628272e58486b200a38fadf525131c006091
ConorCurry <conorcurry@gmail.com>
2016-10-16 16:40:30 +0200

    Removed experimental code remains, Gadfly display (was causing atom problems)

--- agro.jl
+++ agro.jl
@@ -4,31 +4,6 @@ type MapTile{T}
   moisture::T
   fertility::T
 end
-#MapTile{T}() = MapTile{T}(rand(typemin(T):typemax(T)), rand(typemin(T):typemax(T)))
-#=
-function main()
-  gamemap = [MapTile() for x=0:10, y=0:10]
-  print(typeof(gamemap))
-end
-
-function zoom(tile::MapTile)::Array
-end
-
-main()
-=#
-
-#=
-function randmap(x::Int, y::Int, mu=10, st=10/3)
-  genMap = (randn(x,y) .* st) .+ mu
-  for i in 1:length(genMap)
-    while genMap[i] < 0 || 20 < genMap[i]
-      genMap[i] = randn() * st + mu
-    end
-  end
-  println(mean(genMap), "---", std(genMap))
-  return genMap
-end
-=#
 
 function scaledRandn(st::Real, mu::Real, T::DataType)::T
   r = zero(Float16)
@@ -40,7 +15,7 @@ function scaledRandn(st::Real, mu::Real, T::DataType)::T
   return x
 end
 
-function randmap2(x::Int, y::Int, T=UInt8::DataType)
+function randmap(x::Int, y::Int, T=UInt8::DataType)
   lo = typemin(T)
   hi = typemax(T)
   mu = (hi - lo) / 2
@@ -60,12 +35,4 @@ function randmap2(x::Int, y::Int, T=UInt8::DataType)
   return genMap
 end
 
-#@time boundedNorm = randmap(1000, 1000)
-@time boundedNorm = randmap2(100, 100)
-#@show boundedNorm
-#@show [e.moisture for e in boundedNorm]
-
-using Gadfly
-myplot = plot(x=[e.moisture for e in boundedNorm], Geom.histogram)
-draw(PNG("myplot2.png", 4inch, 3inch), myplot)
-println("Done")
+@time boundedNorm = randmap(100, 100)
